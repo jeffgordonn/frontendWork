435fe15be9592fc383614b6e0216da7ab2654957
jeffgordonn <jdgordonn@gmail.com>
2024-06-30 16:13:18 -0400

    Module 4 solution added

---
+++ mod-4-js/SpeakGoodBye.js
@@ -0,0 +1,13 @@
+(function (name) {
+
+var byeSpeaker = new Object();
+
+var speakWord = "Good Bye";
+
+byeSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
+}
+
+window.byeSpeaker = byeSpeaker;
+
+})();
